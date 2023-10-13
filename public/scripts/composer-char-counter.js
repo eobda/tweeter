b93490fe9ecd89799e37f777c1346ccc28196afa
maddie <lee.maddie@gmail.com>
2023-10-13 19:08:24 -0400

    Test targeting tweet-text textarea with focus event

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
   console.log('composer-char-counter has loaded');
+  
+  $('#tweet-text').on('focus', function(event) {
+    console.log('event:', event);
+  });
 });
