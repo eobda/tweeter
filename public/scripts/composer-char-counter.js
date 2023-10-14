a00d05c40fd810f7bd95533df5bbbe7d565510f0
maddie <lee.maddie@gmail.com>
2023-10-14 18:03:27 -0400

    Add .css method to turn counter red if character limit is exceeded

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,5 +7,10 @@ $(document).ready(function() {
     const currentChars = $composer.val().length;
     const $counter = $composer.next().children().filter('.counter');
     $counter.text(charLimit - currentChars);
+
+    // Turn counter red if counter is negative
+    if ((charLimit - currentChars) < 0) {
+      $counter.css('color', '#FF0000');
+    }
   });
 });
