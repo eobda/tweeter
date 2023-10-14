75160afabae29f93b27e612f8679cd672a211030
maddie <lee.maddie@gmail.com>
2023-10-14 15:14:36 -0400

    Add jquery syntax to assigned composer variable and test getting val() and length of val()

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   console.log('composer-char-counter has loaded');
   
   $('#tweet-text').on('input', function() {
-    const $composer = this;
-    console.log($composer);
+    const $composer = $(this);
+    console.log('Value of $composer:', $composer.val());
+    console.log('Length of value of $composer:', $composer.val().length);
   });
 });
