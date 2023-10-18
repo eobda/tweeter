30c8d0eca35fbcf5c282717b3d790b784cffb827
maddie <lee.maddie@gmail.com>
2023-10-18 16:15:13 -0400

    Create createTweetElement function within jQuery document ready call

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,7 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function() {
 
+  const createTweetElement = function(data) {
+    const $tweet = $(`<article class="tweet">Hello world</article>`);
+    return $tweet;
+  };
+});
