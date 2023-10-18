27b23ccdf39d4febdf0b814fe82a81ab5b83b0c7
maddie <lee.maddie@gmail.com>
2023-10-18 16:40:09 -0400

    Implement and test renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -26,22 +26,38 @@ $(document).ready(function() {
     return $tweet;
   };
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-"user": {
-  "name": "Newton",
-  "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-"content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-"created_at": 1461116232227
-}
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweets = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
-const $tweet = createTweetElement(tweetData);
+  const renderTweets = function(data) {
+    for (const tweetData of data) {
+      const $tweet = createTweetElement(tweetData);
+      $('#tweets-container').append($tweet);
+    };
+  };
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet);
+  renderTweets(tweets);
 });
