746f2ee68bc8d1a88de8bc6fec9894e29e765db1
maddie <lee.maddie@gmail.com>
2023-10-18 16:40:45 -0400

    Reorder functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,31 +1,5 @@
 $(document).ready(function() {
 
-  const createTweetElement = function(data) {
-    const $tweet = $(`<article class="tweet">
-      <header>
-        <div>
-          <img src="${data.user.avatars}" />
-          <span>${data.user.name}</span>
-        </div>
-        <div class="username">
-          ${data.user.handle}
-        </div>
-      </header>
-      <div>
-        <p>${data.content.text}</p>
-      </div>
-      <footer>
-        <div>
-          ${data.created_at}
-        </div>
-        <div class="icons">
-          <i class="fa-solid fa-flag"></i> <i class="fa-solid fa-retweet"></i> <i class="fa-solid fa-heart"></i>
-        </div>
-      </footer>
-    </article>`);
-    return $tweet;
-  };
-
   // Test / driver code (temporary). Eventually will get this from the server.
   const tweets = [
     {
@@ -57,6 +31,32 @@ $(document).ready(function() {
       const $tweet = createTweetElement(tweetData);
       $('#tweets-container').append($tweet);
     };
+  };
+
+  const createTweetElement = function(data) {
+    const $tweet = $(`<article class="tweet">
+      <header>
+        <div>
+          <img src="${data.user.avatars}" />
+          <span>${data.user.name}</span>
+        </div>
+        <div class="username">
+          ${data.user.handle}
+        </div>
+      </header>
+      <div>
+        <p>${data.content.text}</p>
+      </div>
+      <footer>
+        <div>
+          ${data.created_at}
+        </div>
+        <div class="icons">
+          <i class="fa-solid fa-flag"></i> <i class="fa-solid fa-retweet"></i> <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`);
+    return $tweet;
   };
 
   renderTweets(tweets);
