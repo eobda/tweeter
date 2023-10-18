1516260fa6a92e7e953e16b95accf84014945d31
maddie <lee.maddie@gmail.com>
2023-10-18 16:24:31 -0400

    Add tweet template HTML to function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,28 @@
 $(document).ready(function() {
 
   const createTweetElement = function(data) {
-    const $tweet = $(`<article class="tweet">Hello world</article>`);
+    const $tweet = $(`<article class="tweet">
+      <header>
+        <div>
+          <img src="${data.user.avatars}" />
+          <span>${data.user.name}</span>
+        </div>
+        <div class="username">
+          ${data.user.handle}
+        </div>
+      </header>
+      <div>
+        <p>${data.user.content}s</p>
+      </div>
+      <footer>
+        <div>
+          ${data.user.created_at}
+        </div>
+        <div class="icons">
+          <i class="fa-solid fa-flag"></i> <i class="fa-solid fa-retweet"></i> <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`);
     return $tweet;
   };
 });
