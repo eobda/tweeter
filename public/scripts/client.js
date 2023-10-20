4997b3a097503cdd8ed26cc49eea211253d2c3d9
maddie <lee.maddie@gmail.com>
2023-10-19 22:56:30 -0400

    Remove test code for renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,31 +1,5 @@
 $(document).ready(function() {
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweets = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   const renderTweets = function(data) {
     for (const tweetData of data) {
       const $tweet = createTweetElement(tweetData);
@@ -74,8 +48,6 @@ $(document).ready(function() {
       return `${daysAgo} days ago`;
     }
   };
-
-  renderTweets(tweets);
 
   // Form submission with AJAX
   $('#submit-tweet').on('submit', function(event) {
