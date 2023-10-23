2b173f4c70bb98d856e144f2c324d6567cb20cc2
maddie <lee.maddie@gmail.com>
2023-10-23 12:27:32 -0400

    Use scrollTop() to fix button toggle behaviour by checking window scrollTop() value

--- public/scripts/nav-composer-button.js
+++ public/scripts/nav-composer-button.js
@@ -6,17 +6,22 @@ $(document).ready(function() {
   });
 
   $(window).on('scroll', function(event) {
-    // Make composer button disappear
-    $('#composer-button').css('display', 'none');
-    $('#scroll-up').css('display', 'block');
+    const $scrollVal = $(window).scrollTop();
+
+    if ($scrollVal > 50) {
+      // Make composer button disappear
+      $('#composer-button').css('display', 'none');
+      $('#scroll-up').css('display', 'block');
+    } else {
+      $('#composer-button').css('display', 'block');
+      $('#scroll-up').css('display', 'none');
+    };
+  
   });
 
   $('#scroll-up').on('click', function(event) {
     $('html').animate({scrollTop: 0}, '300');
     $('section.new-tweet').slideDown();
     $('#tweet-text').focus();
-    // Make composer button appear and scroll up button disappear
-    $('#composer-button').css('display', 'block');
-    $('#scroll-up').css('display', 'none');
   });
 });
